67ac428a8a74d2c1fb06805f29e0a18659793b7a
devdev <51432531+devintaule@users.noreply.github.com>
2022-09-06 23:27:38 +0800

    commit in js

---
+++ javascript.js
@@ -0,0 +1,10 @@
+var fname = "Devin";
+var lname = "Taule"
+var age = 26;
+
+function whoAmI (myFname, myLname, myAge) {
+    console.log(`
+        Hi, I'm ${myFname} (aka ${myLname}),
+        and I'm ${myAge} years old.
+    `);
+}
